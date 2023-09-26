ff0a9bf2491de6397686c6974b59a3e117b298b5
Lyjia <me@lyjia.us>
2023-09-25 17:22:11 -0700

    flesh it out

--- llamacpp-chat2.py
+++ llamacpp-chat2.py
@@ -15,23 +15,35 @@ from configparser import ConfigParser
 from pathlib import Path
 import os
 
+
+def msg(str):
+    print(str)
+
+
 #############
 # Constants #
 #############
 VERSION = "0.0"
 
-CFG_DEFAULT = "defaults"
-CFG_PROMPT_FORMATS = "prompt.formats"
-CFG_MODELS = "models"
+CFG_DEFAULT = "DEFAULTS"
+CFG_PROMPT_FORMATS = "PROMPT.FORMATS"
+CFG_MODELS = "MODELS"
 
 CFG_DEF_QTYTOKES = "qtytokens"
 CFG_DEF_LLAMABIN = "llamabin"
+CFG_DEF_MODELPATH = "modelpath"
+
+CFG_MOD_PATH = "path"
+CFG_MOD_DESC = "desc"
+CFG_MOD_FMT = 'promptformat'
+CFG_MOD_GQA = "gqa"
 
 DEFAULT_CONFIG_PATH = os.path.expanduser("~/.config/simple-llamacpp/config.ini")
 
 DEFAULTS = {
-    CFG_DEF_LLAMABIN: '~/src/llama.cpp/main',
-    CFG_DEF_QTYTOKES: 256
+    CFG_DEF_LLAMABIN:  '~/src/llama.cpp/main',
+    CFG_DEF_QTYTOKES:  256,
+    CFG_DEF_MODELPATH: '~/data/ml/LLM'
 }
 
 PROMPT_FORMATS = {
@@ -44,6 +56,7 @@ PROMPT_FORMATS = {
 #################
 config = ConfigParser()
 
+
 class GenerateDefaultConfigAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         global config
@@ -53,20 +66,20 @@ class GenerateDefaultConfigAction(Action):
         # some examples for specifying models
         models = {
             'llama2-13b-chat-q8':  {
-                'path':         'llama-2-13b-chat/llama-2-13b-chat.ggmlv3.q8_0.bin',
-                'desc':         'LlaMA2 13b-chat, q8_0',
-                'promptformat': 'llama2'
+                CFG_MOD_PATH: 'llama-2-13b-chat/llama-2-13b-chat.ggmlv3.q8_0.bin',
+                CFG_MOD_DESC: 'LlaMA2 13b-chat, q8_0',
+                CFG_MOD_FMT:  'llama2'
             },
             'llama2-70b-chat-q41': {
-                'path':         'llama-2-70b-chat/llama-2-70b-chat.ggmlv3.q4_1.bin',
-                'desc':         'LlaMa2 70b-chat, q4_1 (gqa=8)',
-                'promptformat': 'llama2',
-                'gqa':          '8'
+                CFG_MOD_PATH: 'llama-2-70b-chat/llama-2-70b-chat.ggmlv3.q4_1.bin',
+                CFG_MOD_DESC: 'LlaMa2 70b-chat, q4_1 (gqa=8)',
+                CFG_MOD_FMT:  'llama2',
+                CFG_MOD_GQA:  '8'
             },
             'nous-13b-q41':        {
-                'path':         'Nous-Hermes-Llama2-13b/nous-hermes-llama2-13b.ggmlv3.q4_1.bin',
-                'desc':         'Nous-Hermes 13b q4_1',
-                'promptformat': 'alpaca'
+                CFG_MOD_PATH: 'Nous-Hermes-Llama2-13b/nous-hermes-llama2-13b.ggmlv3.q4_1.bin',
+                CFG_MOD_DESC: 'Nous-Hermes 13b q4_1',
+                CFG_MOD_FMT:  'alpaca'
             }
         }
 
@@ -81,21 +94,19 @@ class GenerateDefaultConfigAction(Action):
             values = DEFAULT_CONFIG_PATH
 
         dest = values
-        print("Generating default config and saving it to: {}".format(dest))
 
         os.makedirs(Path(dest).parent, exist_ok=True)
 
         with open(dest, 'w') as configfile:
             config.write(configfile)
 
-        print("All done!")
-        print("Please go to {} and add your models to the model library using the provided examples")
+        msg("Please add your models to the model library in {} by using the provided examples".format(dest))
         exit(0)
 
 
 class PrintListOfModelsAction(Action):
     def __call__(self, parser, namespace, value, option_string=None):
-        print("Hello!")
+        msg("Hello!")
         exit(0)
 
 
@@ -136,7 +147,8 @@ parser.add_argument("-l", "--listmodels",
 
 parser.add_argument("--generate-default-config",
                     help="Generate a default configuration file for this script and write it to the path provided, "
-                         "then exit. If no path is given, a new config file will be written to {}".format(DEFAULT_CONFIG_PATH),
+                         "then exit. If no path is given, a new config file will be written to {}".format(
+                        DEFAULT_CONFIG_PATH),
                     default=DEFAULT_CONFIG_PATH,
                     action=GenerateDefaultConfigAction,
                     metavar='CONFIG',
@@ -144,14 +156,59 @@ parser.add_argument("--generate-default-config",
 
 args = parser.parse_args()
 
-#######
-# Go! #
-#######
+##############
+# Validation #
+##############
 
+# config file
 config_file = DEFAULT_CONFIG_PATH
 if args.config is None:
     config_file = args.config
 
-model = args.model
+msg("Loading config from {}".format(config_file))
+config.read(config_file)
 
-if os.exists(args.prompt):
+# model file
+model = args.model
+modelhive = "{}.{}".format(CFG_MODELS, model)
+if modelhive in config:
+    model_hash = config[modelhive]
+    model_path = os.path.join(config[CFG_DEF_MODELPATH], model_hash[CFG_MOD_PATH])
+    if os.exist(model_path):
+        msg("Using model at {}".format(model_path))
+    else:
+        msg("ERROR: Model file at {} does not exist!".format(model_hash[CFG_MOD_PATH]))
+        exit(-2)
+else:
+    if args.model is None:
+        msg("ERROR: You must specify a model to use with -m.".format(model))
+    else:
+        msg("ERROR: Model {} does not exist in the model library!".format(model))
+    exit(-3)
+
+# prompt
+if args.prompt:
+    if os.exists(args.prompt):
+        prompt_file = args.prompt
+        with open(prompt_file, 'r') as f:
+            prompt = f.read()
+        msg("Loaded prompt from {}...".format(prompt_file))
+    else:
+        prompt = args.prompt
+    msg("Using prompt of length {}.".format(len(prompt)))
+else:
+    msg("ERROR: you must specify a prompt for prompt file!")
+    exit(-1)
+
+# system prompt
+if args.sysprompt:
+    if os.exists(args.sysprompt):
+        sysprompt_file = args.sysprompt
+        with open(sysprompt_file, 'r') as f:
+            sysprompt = f.read()
+        msg("Loaded system prompt from {}...".format(prompt_file))
+    else:
+        sysprompt = args.sysprompt
+    msg("Using system prompt of length {}.".format(len(sysprompt)))
+else:
+    msg("No system prompt specified.")
